b009dbc2222678624ef0a3aeeea850d74d0d7337
sam.tsai <sam.tsai@insyde.com>
2021-04-05 15:58:46 +0800

    fix path issue

--- put_data_out_to_file.c
+++ put_data_out_to_file.c
@@ -4,14 +4,14 @@
 
 #define Len_s    2
 unsigned int s[Len_s] = {0xcdbd0acf, 0xdcabab}; //{11111111111111111011110110110011, 11111111111111110011101100000000};
-unsigned char s1[8] = {0x30, 0x31, 0x31};
+unsigned char s1[8] = {0x30, 0x31, 0x31, 0x30, 0x30, 0x31};
 
 int File_Out(void){
 
     printf("p1 = %x, p2 = %x \n", s[0], s[1]);
 
 
-    FILE *pf = fopen("/home/sam.tsai/code/Test_Simple_Function/f1.txt","w");
+    FILE *pf = fopen("./f1.txt","w");
     fwrite(s1, 1, sizeof(s1), pf);
     
     fclose(pf);
