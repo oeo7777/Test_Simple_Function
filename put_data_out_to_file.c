ba22ca0b595dba2ad459ff7315440e3621e67006
sam.tsai <sam.tsai@insyde.com>
2021-04-05 15:54:59 +0800

    char can store in the ASCII type

--- put_data_out_to_file.c
+++ put_data_out_to_file.c
@@ -4,6 +4,7 @@
 
 #define Len_s    2
 unsigned int s[Len_s] = {0xcdbd0acf, 0xdcabab}; //{11111111111111111011110110110011, 11111111111111110011101100000000};
+unsigned char s1[8] = {0x30, 0x31, 0x31};
 
 int File_Out(void){
 
@@ -11,7 +12,7 @@ int File_Out(void){
 
 
     FILE *pf = fopen("/home/sam.tsai/code/Test_Simple_Function/f1.txt","w");
-    fwrite(s, 1, sizeof(s), pf);
+    fwrite(s1, 1, sizeof(s1), pf);
     
     fclose(pf);
 
